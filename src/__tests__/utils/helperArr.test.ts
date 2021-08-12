c325e0cee507b813b1b235179b0f958318719853
levinhax <levin1229@163.com>
2021-08-12 19:42:31 +0800

    feat: Jest单元测试

---
+++ src/__tests__/utils/helperArr.test.ts
@@ -0,0 +1,11 @@
+import { isAllEqual, isRepeat } from '../../utils/helperArr'
+
+describe('数组函数测试', () => {
+  test('array isAllEqual', () => {
+    expect(isAllEqual([1, 2, 3, 3, 4])).toBe(true)
+  })
+
+  test('array isRepeat', () => {
+    expect(isRepeat([1, 2, 3, 3, 4])).toBe(true)
+  })
+})
