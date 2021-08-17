71f042a9fbac3115ad49df04768c8017dc70fcd1
levinhax <levin1229@163.com>
2021-08-17 20:05:47 +0800

    feat: 卡片

--- src/__tests__/utils/helperArr.test.ts
+++ src/__tests__/utils/helperArr.test.ts
@@ -2,7 +2,7 @@ import { isAllEqual, isRepeat } from '../../utils/helperArr'
 
 describe('数组函数测试', () => {
   test('array isAllEqual', () => {
-    expect(isAllEqual([1, 2, 3, 3, 4])).toBe(true)
+    expect(isAllEqual([1, 2, 3, 3, 4])).toBe(false)
   })
 
   test('array isRepeat', () => {
