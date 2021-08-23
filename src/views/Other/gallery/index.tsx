e08dbcea9e5bcdca13d8fefa4f6a74b139e85994
levinhax <levin1229@163.com>
2021-08-23 19:24:24 +0800

    feat: css Modules

--- src/views/Other/gallery/index.tsx
+++ src/views/Other/gallery/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import classes from './index.module.less'
 
 function OtherGallery() {
-  return <div>画廊</div>
+  return (
+    <div className={classes['other-gallery-wrapper']}>
+      <h3 className={classes.title}>画廊</h3>
+    </div>
+  )
 }
 
 export default OtherGallery
