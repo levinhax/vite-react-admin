1e4518eddcc4ac8efb77c8b45fc73fbc6b24f637
levinhax <levin1229@163.com>
2022-01-05 10:27:26 +0800

    feat: 播放源地址切换

--- src/components/liveVideo/index.tsx
+++ src/components/liveVideo/index.tsx
@@ -3,84 +3,92 @@ import React, { useRef, useEffect } from 'react'
 import Videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 
-interface IProps {
-  liveVideoConfig: {
-    type: string
-    origin: string
+type VideoJsPlayer = /*unresolved*/ any
+
+interface ISource {
+  type: string
+  src: string
+}
+
+type IProps = {
+  options: {
     autoPlay: boolean
+    controls: boolean
+    responsive: boolean
+    fluid: boolean
+    sources: Array<ISource>
   }
+  // eslint-disable-next-line no-unused-vars
+  onReady(player: VideoJsPlayer): void
 }
 
 function LiveVideo(props: IProps) {
-  const { liveVideoConfig } = props
-  let myPlayer: any
-  const videoRef: any = useRef() //拿到DOM容器
-  // const [liveConfig, setLiveConfig] = useState({
-  //   type: 'application/x-mpegURL',
-  //   origin: 'http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8',
-  //   autoPlay: true,
-  // })
+  const { options, onReady } = props
+
+  const videoRef: any = useRef() // 拿到DOM容器
+  const playerRef: any = useRef()
 
   useEffect(() => {
-    initVideo()
-  }, [liveVideoConfig])
+    // make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current
+      if (!videoElement) return
+
+      const player = (playerRef.current = Videojs(videoElement, options, () => {
+        console.log('onPlayerReady')
+        onReady && onReady(player)
+      }))
+      console.log('player: ', player)
+    } else {
+      // you can update player here [update player through props]
+      const player = playerRef.current
+      player.autoplay(options.autoPlay)
+      player.src(options.sources)
+    }
+  }, [options, videoRef])
 
   useEffect(() => {
-    // 在页面销毁生命周期里调用,否则页面销毁也会持续请求直播流
-    return () => myPlayer.dispose()
-  }, [])
+    const player = playerRef.current
 
-  const initVideo = () => {
-    // let myPlayer = Videojs(document.getElementById('liveVideo'), {
-    //   controls: true,
-    //   //自动播放属性,muted:静音播放
-    //   autoplay: liveConfig.autoPlay,
-    //   //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
-    //   preload: 'auto',
-    //   //设置视频播放器的显示宽度（以像素为单位）
-    //   // width: "800px",
-    //   //设置视频播放器的显示高度（以像素为单位）
-    //   // height: "400px",
-    //   controlBar: {
-    //     playToggle: true,
-    //   },
-    // })
-    // console.log('myPlayer: ', myPlayer)
+    return () => {
+      if (player) {
+        player.dispose()
+        playerRef.current = null
+      }
+    }
+  }, [playerRef])
 
-    // if (myPlayer) {
-    //   myPlayer.dispose()
-    // }
+  // useEffect(() => {
+  //   initVideo()
+  // }, [liveVideoConfig])
 
-    myPlayer = Videojs(videoRef.current, {
-      controls: true,
-      //自动播放属性,muted:静音播放
-      autoplay: liveVideoConfig.autoPlay,
-      //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
-      preload: 'auto',
-      //设置视频播放器的显示宽度（以像素为单位）
-      // width: "800px",
-      //设置视频播放器的显示高度（以像素为单位）
-      // height: "400px",
-      controlBar: {
-        playToggle: true,
-      },
-    })
-    console.log('myPlayer: ', myPlayer)
-  }
+  // useEffect(() => {
+  //   // 在页面销毁生命周期里调用,否则页面销毁也会持续请求直播流
+  //   return () => myPlayer.dispose()
+  // }, [])
+
+  // const initVideo = () => {
+  //   // let myPlayer = Videojs(document.getElementById('liveVideo'), {
+  //   //   controls: true,
+  //   //   //自动播放属性,muted:静音播放
+  //   //   autoplay: liveConfig.autoPlay,
+  //   //   //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
+  //   //   preload: 'auto',
+  //   //   //设置视频播放器的显示宽度（以像素为单位）
+  //   //   // width: "800px",
+  //   //   //设置视频播放器的显示高度（以像素为单位）
+  //   //   // height: "400px",
+  //   //   controlBar: {
+  //   //     playToggle: true,
+  //   //   },
+  //   // })
+  //   // console.log('myPlayer: ', myPlayer)
+  // }
 
   return (
     <div className="video-wrap">
       {/* 可以直播的视频标签 */}
-      <video
-        ref={videoRef}
-        id="liveVideo"
-        className="video-js vjs-big-play-centered vjs-fluid"
-        style={{ width: '100%' }}
-        controls
-        preload="auto"
-      >
-        {liveVideoConfig.origin && <source type={liveVideoConfig.type} src={liveVideoConfig.origin} />}
-      </video>
+      <video ref={videoRef} className="video-js vjs-big-play-centered" />
     </div>
   )
 }
--- src/views/Other/video/components/live/index.tsx
+++ src/views/Other/video/components/live/index.tsx
@@ -1,44 +1,92 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import LiveVideo from '@/components/liveVideo'
 import { Row, Col, Select } from 'antd'
 
 const { Option } = Select
 
 function LiveVideoTab() {
+  const playerRef: any = useRef()
   const [liveVideoConfig, setLiveVideoConfig] = useState({
-    type: 'application/x-mpegURL',
-    origin: 'http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8',
     autoPlay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        type: 'application/x-mpegURL',
+        src: 'http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8',
+      },
+    ],
   })
 
   const [sourceList] = useState([
     {
       name: 'CCTV1',
       id: 1,
-      type: 'application/x-mpegURL',
-      origin: 'http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8',
       autoPlay: true,
+      controls: true,
+      responsive: true,
+      fluid: true,
+      sources: [
+        {
+          type: 'application/x-mpegURL',
+          src: 'http://cctvalih5ca.v.myalicdn.com/live/cctv1_2/index.m3u8',
+        },
+      ],
     },
     {
       name: 'CCTV2',
       id: 2,
-      type: 'application/x-mpegURL',
-      origin: 'http://cctvalih5ca.v.myalicdn.com/live/cctv2_2/index.m3u8',
       autoPlay: true,
+      controls: true,
+      responsive: true,
+      fluid: true,
+      sources: [
+        {
+          type: 'application/x-mpegURL',
+          src: 'http://cctvalih5ca.v.myalicdn.com/live/cctv2_2/index.m3u8',
+        },
+      ],
     },
     {
-      name: '测试',
-      id: 100,
-      type: 'application/x-mpegURL',
-      origin: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+      name: 'CCTV3',
+      id: 3,
       autoPlay: true,
+      controls: true,
+      responsive: true,
+      fluid: true,
+      sources: [
+        {
+          type: 'application/x-mpegURL',
+          src: 'http://cctvalih5ca.v.myalicdn.com/live/cctv3_2/index.m3u8',
+        },
+      ],
     },
   ])
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player
+
+    // you can handle player events here
+    player.on('waiting', () => {
+      console.log('player is waiting')
+    })
+
+    player.on('dispose', () => {
+      console.log('player will dispose')
+    })
+  }
 
   const handleSelectChange = (value: number) => {
     const curSource = sourceList.find(item => item.id === value)
     if (curSource) {
+      // if (!playerRef.current) {
+      //   return
+      // }
       setLiveVideoConfig(curSource)
+      // [update player through instance's api]
+      // playerRef.current.src([{ src: 'http://ex.com/video.mp4', type: 'video/mp4' }])
+      // playerRef.current.autoplay(false)
     }
   }
 
@@ -54,7 +102,7 @@ function LiveVideoTab() {
     <div className="videos-wrap">
       <Row gutter={16}>
         <Col xs={12} sm={12} md={12} lg={12} xl={10}>
-          <LiveVideo liveVideoConfig={liveVideoConfig} />
+          <LiveVideo options={liveVideoConfig} onReady={handlePlayerReady} />
           <div style={{ paddingTop: '24px' }}>
             <Select defaultValue={1} style={{ width: 160 }} onChange={handleSelectChange}>
               {renderOptions}
